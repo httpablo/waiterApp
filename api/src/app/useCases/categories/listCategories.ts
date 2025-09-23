7f74cbb91cac8764c128c4be49a2e1a03da51495
httpablo <pablobrith@gmail.com>
2025-09-23 16:07:33 -0300

    Added api directory

---
+++ api/src/app/useCases/categories/listCategories.ts
@@ -0,0 +1,8 @@
+import type { Request, Response } from "express";
+import { Category } from "../../models/Category.js";
+
+export async function listCategories(req: Request, res: Response) {
+    const categories = await Category.find();
+
+    res.json(categories);
+}
---
+++ api/src/index.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import mongoose from "mongoose";
+
+import { router } from "./router.js";
+
+mongoose
+    .connect("mongodb://localhost:27017")
+    .then(() => {
+        const app = express();
+        const port = 3001;
+
+        app.use(router);
+
+        app.listen(port, () => {
+            console.log(`Server is running on http://localhost:${port}`);
+        });
+    })
+    .catch((error) => {
+        console.error("Error connecting to MongoDB:", error);
+    });
