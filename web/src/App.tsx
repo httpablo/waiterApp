e3d4ad4b290eeb80004a363f877c2ac47f0bce34
Pablo Brito <pablobrith@gmail.com>
2025-09-25 19:12:09 -0300

    react starter structure

---
+++ web/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { GlobalStyles } from "./styles/GlobalStyles";
+
+export function App() {
+    return (
+        <>
+            <GlobalStyles />
+            <h1>Hello World</h1>
+        </>
+    );
+}
